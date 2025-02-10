faf886a3aff7745bc2e6ef29730ed6f501d9b7e4
Licodeao <1019805199@qq.com>
2025-02-10 16:20:11 +0800

    feat: 将px换成rpx

--- miniprogram/pages/me/index.js
+++ miniprogram/pages/me/index.js
@@ -2,24 +2,4 @@ Page({
   data: {
     defaultName: "未登陆"
   },
-  handleLogin(e) {
-    console.log(e)
-    let cloudID = e.detail.cloudID
-    if (!cloudID) {
-      wx.showToast({
-        title: '用户未授权',
-      })
-      return
-    }
-    wx.cloud.callFunction({
-      name: 'getLogin',
-      data: {
-        phone: wx.cloud.CloudID(e.detail.cloudID)
-      }
-    }).then(res => {
-      console.log('success', res)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
 })
